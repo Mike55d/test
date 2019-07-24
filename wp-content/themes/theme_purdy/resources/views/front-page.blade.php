269b17fe5dd374f799c1d6557b5d5324377cb955
Alonso Quesada <alonso.quesada@doubledigit.com>
2019-07-24 14:37:54 -0600

    PML-13 Favicon & font files

--- wp-content/themes/theme_purdy/resources/views/front-page.blade.php
+++ wp-content/themes/theme_purdy/resources/views/front-page.blade.php
@@ -1,9 +1,15 @@
+{{--
+  Template Name: Front Page
+--}}
+
+
 @extends('layouts.app')
 
 @section('content')
   <div class="page-home">
     <div class="container">
         <div class="col-12">
+            <h1>Hello from Front Page</h1>
             <h1>@title</h1> 
         </div>
     </div>
