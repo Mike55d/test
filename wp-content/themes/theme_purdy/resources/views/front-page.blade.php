e0b51d53d2952f046aef08883ddc1143e897fdc4
Alonso Quesada <alonso.quesada@doubledigit.com>
2019-07-27 21:29:04 -0600

    Icons and Images

--- wp-content/themes/theme_purdy/resources/views/front-page.blade.php
+++ wp-content/themes/theme_purdy/resources/views/front-page.blade.php
@@ -9,8 +9,15 @@
   <div class="page-home">
     <div class="container">
         <div class="col-12">
-            <h1>Hello from Front Page</h1>
-            <h1>@title</h1> 
+            <h1>H1 style</h1>
+            <h2>H2 style</h2> 
+            <h3>H3 style</h3> 
+            <h4>H4 style</h4> 
+            <h5>H2 style</h5> 
+            <p>Simple paragraph</p>
+            <a href="" class="button button-primary">Button primary</a>
+            <br>
+            <a href="" class="button button-arrow">Ver más <i class="icn icn-1 icn-chevron"></i></a>
         </div>
     </div>
   </div>
