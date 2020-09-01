8255a5002a1c520a369b24d90b2facd8b5506e0b
Pablo Puente <zetapuente@hotmail.com>
2020-09-01 23:19:33 +0000

    Merged in feature/cambiar-url (pull request #52)
    url

--- wp-content/themes/theme_purdy/resources/views/partials/header.blade.php
+++ wp-content/themes/theme_purdy/resources/views/partials/header.blade.php
@@ -24,7 +24,7 @@
         <li class="nav-item"><a href="<?php echo get_navlink_href('#people'); ?>">@php _e('Gente Purdy') @endphp</a></li>
         <li class="nav-item"><a href="<?php echo get_navlink_href('#locations'); ?>">@php _e('Encontranos') @endphp</a></li>
         @if(get_locale() == "es_CR") 
-          <li class="nav-item {{ $pagename == "purdy-mobility-challenge" ? "active" : "" }}"><a href="{{ get_navlink_href('purdy-mobility-challenge') }}">{{ _e('Purdy Mobility') }}</a></li>
+          <li class="nav-item {{ $pagename == "purdy-mobility-challenge" ? "active" : "" }}"><a href="{{ get_navlink_href('challenge') }}">{{ _e('Purdy Mobility') }}</a></li>
         @endif
         <li class="nav-item"><a href="<?php echo get_navlink_href('#contact'); ?>">@php _e('Contáctanos') @endphp</a></li>
       </ul>
